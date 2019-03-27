d50a0d0f7a06e40db7a63f69320943861560396d
KaidDuong <cooleen.cl@gmail.com>
2019-03-28 01:11:31 +0700

    enter

--- Inc/Config.php
+++ Inc/Config.php
@@ -3,6 +3,11 @@
 if(!defined('__CONFIG__')){
     echo " You haven't a config file";
 }
+//Session are always turned on
+if(!isset($_SESSION)){
+    session_start();
+    }
+
 //our config is below
 //allow errors
 error_reporting(-1);
@@ -10,6 +15,10 @@ ini_set('display_errors','On');
 
 //decleared variable ALLOW FOOTER
 define('ALLOW FOOTER',true);
-
-
+//include the DB.php file
+include_once "../Database/Classes/Data.php";
+include_once"../Database/Classes/Filter.php";
+include_once"../Database/Classes/User.php";
+include_once"../Database/Classes/Page.php";
+$con= Data::getConnection();
 ?>
--- Inc/Footer.php
+++ Inc/Footer.php
@@ -10,4 +10,4 @@ if(!defined('ALLOW FOOTER'))
  <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script> 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>  
-
+<script src="../Assets/Js/Main.js"></script>
